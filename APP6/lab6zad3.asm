a3e82917bd32f100ac21c1ad96add35decc64f48
s24306 <s24306@pjwstk.edu.pl>
2022-11-06 18:42:57 +0100

    finish lab 6

---
+++ APP6/lab6zad3.asm
@@ -0,0 +1,23 @@
+org 100h
+
+jmp loop
+
+loop:
+mov AH, 00h  	;
+int 16h	;get key from the user
+
+cmp AL, 51h	;check if key is "Q"
+JE end		;
+
+mov AH, 02h	;
+mov DL, AL	;
+int 21h	;print user input
+
+mov DL, 0Ah	;
+int 21h	;newline
+
+jmp loop
+
+end:
+MOV AX, 4C00h	;
+int 21h	;exit prog
